b9042433e1567897ec9d23454111f8d60e22fbe6
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-11-29 12:19:57 +0200

    multi bar subplots_chart for_PhD + Adding the chart

--- Questions/Question_1/top_phD_position.py
+++ Questions/Question_1/top_phD_position.py
@@ -29,72 +29,11 @@ def top_phd_position_for_men_and_women(df):
 
     return res_female, res_male
 
-
 # **************************************************************************************************************
-# Function  name: fix_str- helper function
+# Function  name: break_into_separate_word
 # input:
 # return value:
 # ***************************************************************************************************************
-def fix_str(my_label):
-    pos = my_label.find(' ')
-    final_str = my_label[:pos] + '\n' + my_label[pos:]
-    return final_str
-
-
-# **************************************************************************************************************
-# Function  name: multi_bar_plot_chart_for_PhD
-# input:
-# return value:
-# ***************************************************************************************************************
-def multi_bar_subplots_chart_for_PhD(res_female, res_male):
-    # plt.style.use('seaborn')  # This line is responsible for the gray background
-    labels_male = list(res_male.loc[:, 'Job Title'])
-    labels_female = list(res_female.loc[:, 'Job Title'])
-    print('*')
-    my_new_labels_male = []
-    my_new_labels_female = []
-
-    for current_label in labels_male:
-        res = fix_str(current_label)
-        my_new_labels_male.append(res)
-        print('*')
-
-    for current_label in labels_female:
-        res = fix_str(current_label)
-        my_new_labels_female.append(res)
-        print('*')
-
-    Male_PhD = res_male.loc[:, 'count']
-    Female_PhD = res_female.loc[:, 'count']
-    print('*')
-
-    x = np.arange(len(labels_female))
-    width = 0.8
-
-    fontdict_input = {'fontsize': 13, 'weight': 'heavy', 'ha': 'center', 'alpha': 0.9, 'color': 'Gray'}
-
-    fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10, 5))
-    fig.suptitle('PhD Position Gender Comparison in the High-Tech Industry', fontdict=fontdict_input, fontsize=22,
-                 fontname='Franklin Gothic Medium Cond')
-
-    # Chart number 1 - Male
-    axes[0].set_title('Types of positions for male with a Ph.D', fontsize=18, fontdict=fontdict_input,
-                      fontname='Franklin Gothic Medium Cond')
-    axes[0].bar(x - width / 2 + 0.08, Male_PhD, width, label='test1', color='cornflowerblue')
-    axes[0].set_xticks([0, 1, 2, 3, 4, 5], my_new_labels_male, fontsize=12.5)
-
-    # Chart number 2 - Female
-    axes[1].set_title('Types of positions for female with a Ph.D', fontsize=18, fontdict=fontdict_input,
-                      fontname='Franklin Gothic Medium Cond')
-    axes[1].bar(x - width / 2 + 0.08, Female_PhD, width, color='navy')
-    axes[1].set_xticks([0, 1, 2, 3, 4, 5], my_new_labels_female, fontsize=12.5)
-    # axes[1].text(x=1, y=0.5, s='F', ha='left', va='bottom', fontdict=fontdict_input)
-
-    plt.figlegend(loc='upper right', ncol=1, labelspacing=0.4, fontsize=14, bbox_to_anchor=(1.11, 0.9))
-    plt.tight_layout(w_pad=6)
-    plt.show()
-
-
 def break_into_separate_word(labels):
     separate_words_for_each_label = []
     for label in labels:
@@ -103,26 +42,40 @@ def break_into_separate_word(labels):
 
     return separate_words_for_each_label
 
-
-def add_numbers_to_plots(values, ax, idx_gender):
+# **************************************************************************************************************
+# Function  name: add_numbers_to_plots
+# input:
+# return value:
+# ***************************************************************************************************************
+def add_numbers_for_each_bar_chart(values, ax, idx_gender):
     # Add numbers on top of the bars
     for i, value in enumerate(values):
-        ax[idx_gender].text(i, value + 0.1, str(value), ha='center', va='bottom')
+        ax[idx_gender].text(i, value + 0.1, str(value), ha='center', va='bottom',fontname='Franklin Gothic Medium Cond' )
 
-
+# **************************************************************************************************************
+# Function  name: multi_bar_subplots_chart_for_PhD2
+# input:
+# return value:
+# ***************************************************************************************************************
 def multi_bar_subplots_chart_for_PhD2(res_female, res_male):
+
     fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(16, 9))
     data_per_gender = {'female': res_female, 'male': res_male}
     colors = {'female': 'lightpink', 'male': 'lightblue'}
+    fontdict_input = {'fontsize': 13, 'weight': 'heavy', 'ha': 'center', 'alpha': 0.9, 'color': 'Gray'}
+
+    fig.suptitle('PhD Position Gender Comparison in the High-Tech Industry', fontdict=fontdict_input, fontsize=22, fontname='Franklin Gothic Medium Cond')
 
     for idx, gender in enumerate(['female', 'male']):
+
         gender_phd_count = data_per_gender[gender].loc[:, 'count']
         gender_labels = list(res_male.loc[:, 'Job Title'])
-        ax[idx].bar(break_into_separate_word(gender_labels), gender_phd_count, color=colors[gender], width=0.9)
-        ax[idx].set_title(gender.upper())
+        ax[idx].set_title(f'Types of positions for {gender} with a Ph.D', fontsize=18, fontdict=fontdict_input,fontname='Franklin Gothic Medium Cond')
+        ax[idx].bar(break_into_separate_word(gender_labels), gender_phd_count, color=colors[gender], width=0.9 )
 
-        add_numbers_to_plots(gender_phd_count, ax, idx_gender=idx)
 
+        add_numbers_for_each_bar_chart(gender_phd_count, ax, idx_gender=idx)
+        plt.savefig('Gender Comparison.jpg', dpi=250, bbox_inches='tight')
     plt.show()
 
 
@@ -135,6 +88,6 @@ if __name__ == '__main__':
     print(list(df.columns))
 
     res_female, res_male = top_phd_position_for_men_and_women(df)
-    res = pd.concat([res_male, res_female], axis=1)
+    #res = pd.concat([res_male, res_female], axis=1)
     multi_bar_subplots_chart_for_PhD2(res_female, res_male)
     print('*')
