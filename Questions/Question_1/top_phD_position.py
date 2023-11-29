1c494be18d79d044cd2c8bba93d92edb59d3a882
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-11-29 09:26:05 +0200

    multi bar subplots_chart for_PhD

--- Questions/Question_1/top_phD_position.py
+++ Questions/Question_1/top_phD_position.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sys
 from matplotlib.colors import LinearSegmentedColormap # in order to add the gradient color
-import seaborn as sns
+
 
 # Datasets:
 # https://www.kaggle.com/datasets/amirmahdiabbootalebi/salary-by-job-title-and-country
@@ -46,41 +47,48 @@ def fix_str(my_label):
 # return value:
 # ***************************************************************************************************************
 def multi_bar_subplots_chart_for_PhD(res_female, res_male):
-
+    #plt.style.use('seaborn')  # This line is responsible for the gray background
     labels_male = list(res_male.loc[:,'Job Title'])
+    labels_female =list(res_female.loc[:,'Job Title'])
+    print('*')
     my_new_labels_male = []
+    my_new_labels_female = []
 
     for current_label in labels_male:
         res = fix_str(current_label)
         my_new_labels_male.append(res)
         print('*')
 
-    #labels_male = res_male.loc[:,'Job Title'] # df.loc[:k, 'pts']
-    print('*')
-    #['Data\nScientist', 'Research\nScientist', 'Software\nEngineer\nManager', 'Project\nEngineer','Product\nMarketing\nManager', 'Research\nDirector']
-    labels_female =res_female.loc[:,'Job Title']
-    print('*')
-    #['Data\nScientist', 'Research\nScientist', 'Software\nEngineer\nManager', 'Project\nEngineer', 'Product\nMarketing\nManager', 'Research\nDirector']
+
+    for current_label in labels_female:
+        res = fix_str(current_label)
+        my_new_labels_female.append(res)
+        print('*')
+
+
     Male_PhD = res_male.loc[:,'count']
     Female_PhD = res_female.loc[:,'count']
+    print('*')
 
     x = np.arange(len(labels_female))
-    width = 0.5
+    width = 0.8
 
-    plt.figure(figsize=(12, 5))
-
-    plt.subplot(121)
     fontdict_input = {'fontsize': 13, 'weight': 'heavy', 'ha': 'center', 'alpha': 0.9, 'color': 'Gray'}
-    plt.title('Types of positions for male with a Ph.D', fontsize=18, fontdict=fontdict_input,
-              fontname='Franklin Gothic Medium Cond')
-    plt.bar(x - width / 2 + 0.08, Male_PhD, width, label='test1', hatch='^', color=np.array((199, 66, 120)) / 255)
-    plt.xticks([0, 1, 2, 3, 4, 5], my_new_labels_male, fontsize=12.5)
 
-    plt.subplot(122)
-    plt.title('Types of positions for female with a Ph.D', fontsize=18, fontdict=fontdict_input,
-              fontname='Franklin Gothic Medium Cond')
-    plt.bar(x - width / 2 + 0.08, Female_PhD, width, hatch='/', color=np.array((199, 199, 92)) / 255)
-    plt.xticks([0, 1, 2, 3, 4, 5], labels_female, fontsize=12.5)
+    fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10, 5))
+    fig.suptitle( 'PhD Position Gender Comparison in the High-Tech Industry',fontdict=fontdict_input, fontsize=22 , fontname='Franklin Gothic Medium Cond')
+
+    # Chart number 1 - Male
+    axes[0].set_title('Types of positions for male with a Ph.D', fontsize=18, fontdict=fontdict_input,fontname='Franklin Gothic Medium Cond')
+    axes[0].bar(x - width / 2 + 0.08, Male_PhD, width, label='test1', color = 'cornflowerblue')
+    axes[0].set_xticks([0, 1, 2, 3, 4, 5], my_new_labels_male, fontsize=12.5)
+
+    # Chart number 2 - Female
+    axes[1].set_title('Types of positions for female with a Ph.D', fontsize=18, fontdict=fontdict_input,fontname='Franklin Gothic Medium Cond')
+    axes[1].bar(x - width / 2 + 0.08, Female_PhD, width,  color='navy')
+    axes[1].set_xticks([0, 1, 2, 3, 4, 5], my_new_labels_female, fontsize=12.5)
+    #axes[1].text(x=1, y=0.5, s='F', ha='left', va='bottom', fontdict=fontdict_input)
+
 
     plt.figlegend(loc='upper right', ncol=1, labelspacing=0.4, fontsize=14, bbox_to_anchor=(1.11, 0.9))
     plt.tight_layout(w_pad=6)
@@ -89,40 +97,12 @@ def multi_bar_subplots_chart_for_PhD(res_female, res_male):
 
 
 
-
-
 if __name__ == '__main__':
     df = pd.read_csv('../../data/salary_by_job_country/Salary.csv')
     print(list(df.columns))
 
-    names_of_races = df['Race'].unique()
-    # ['White' 'Hispanic' 'Asian' 'Korean' 'Chinese' 'Australian' 'Welsh', 'African American' 'Mixed' 'Black']
-    print(list(names_of_races))
-
-    names_of_countries = df['Country'].unique()
-    print(list(names_of_countries))  # ['UK' 'USA' 'Canada' 'China' 'Australia']
-    print('*')
-
-    names_of_Job_Title = df['Job Title'].unique()
-    # ['Software Engineer' 'Data Analyst' 'Manager' 'Sales Associate' 'Director', 'Marketing Analyst' 'Product Manager'
-    # 'Sales Manager', 'Marketing Coordinator' 'Scientist' 'Software Developer' 'HR Manager', 'Financial Analyst'
-    # 'Project Manager' 'Customer Servic]
-    print(list(
-        names_of_Job_Title))
-    print('*')
-
-    # groups_by_races = df.groupby('Race')
-    # for race, mini_df_by_race in groups_by_races:
-    #     print("The race type is: ", race)
-    #     print(mini_df_by_race)
-    #     print(mini_df_by_race.shape[0])
-    #     print('*')
-    #
-    # data_length = df.shape[0]
-    # print('*')
 
 
-
-    res_female, res_female =top_phd_position_for_men_and_women(df)
-    multi_bar_subplots_chart_for_PhD(res_female ,res_female )
+    res_female, res_male =top_phd_position_for_men_and_women(df)
+    multi_bar_subplots_chart_for_PhD(res_female ,res_male )
     print('*')
