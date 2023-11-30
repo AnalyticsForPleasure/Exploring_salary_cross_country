9b46db82832eb7d9d78be6181edd58de278a4691
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-11-30 06:14:42 +0200

    creating a distributed dot plot for HR department

--- Questions/Question_2/classify_jobs.py
+++ Questions/Question_2/classify_jobs.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.ticker import FuncFormatter
 
+
+def format_with_commas(value, pos):
+    return f'{value:,.0f}'
 
 def creating_a_distributed_dot_plot_for_each_department(df_raw, font_prop):
     # Mean and Median city mileage by make
@@ -11,29 +15,34 @@ def creating_a_distributed_dot_plot_for_each_department(df_raw, font_prop):
     print('*')
 
     # Draw horizontal lines
-    fig, ax = plt.subplots(figsize=(16, 10), dpi=80)
+    fig, ax = plt.subplots(figsize=(16, 9), dpi=80)
     ax.hlines(y=df.index, xmin=0, xmax=df['Salary'].max(), color='gray', alpha=0.5, linewidth=.7, linestyles='dashdot')
     ax.tick_params(labelleft=True, labelright=False, left=True, right=False)
     # Draw the Dots
+    df_raw.sort_values(by=['Salary'], inplace=True)
     groups = df_raw.groupby(['Job Title'])
     for idx, (group_name, mini_df) in enumerate(groups):
         num_circles_per_job_title = mini_df.shape[0]
         median_salary_for_each_group = mini_df['Salary'].median()
-        ax.scatter(y=np.repeat(idx, num_circles_per_job_title), # position over the y-axis
-                   x=mini_df['Salary'],                         # position over the x-axis
+        ax.scatter(y=np.repeat(idx, num_circles_per_job_title),  # position over the y-axis
+                   x=mini_df['Salary'],  # position over the x-axis
                    s=125,
-                   edgecolors='gray', c='w', alpha=0.5)
-        ax.scatter(y=idx, x=median_salary_for_each_group, s=125, c='lightblue')
-    plt.gca().set_xticklabels([f'{x:,.0f}' for x in df_raw['Salary']])
-    # Annotate
-    ax.text(28, 13, "$Blue \; dots \; are \; the \: median$", fontdict={'size': 12}, c='blue')
+                   edgecolors='gray', c='lightblue', alpha=0.8)
+        ax.scatter(y=idx, x=median_salary_for_each_group, s=125, c='mediumblue', alpha=0.6)
+
+    ax.xaxis.set_major_formatter(FuncFormatter(format_with_commas))
+
+    ax.text(28, 13, "$Blue \; dots \; are \; the \: median$", fontdict={'size': 12}, c='blue',
+            fontname=font_prop['fontname'])
+
     # Decorations
-    red_patch = plt.plot([], [], marker="o", ms=10, ls="", mec=None, color='lightblue', label="Median")
-    plt.legend(handles=red_patch)
-    ax.set_title('Salary distribution across HR positions', fontdict={'size': 32}, fontsize=font_prop['fontsize'],fontname=font_prop['fontname'])
-    ax.set_xlabel('Salary  - In U.S dollars( $ )', alpha=0.7,fontsize=22,fontname=font_prop['fontname'] )
+    lightblue_patch = plt.plot([], [], marker="o", ms=10, ls="", mec=None, color='mediumblue', label="Median")
+    plt.legend(handles=lightblue_patch)
+    ax.set_title('Salary distribution across HR positions', fontdict={'size': 42}, fontsize=font_prop['fontsize'],
+                 fontname=font_prop['fontname'])
+    ax.set_xlabel('Salary - In U.S dollars ( $ )', alpha=0.7, fontsize=22, fontname=font_prop['fontname'])
     ax.set_yticks(df.index)
-    ax.set_yticklabels(df['Job Title'].str.title(), fontdict={'horizontalalignment': 'right'}, alpha=0.7,fontname=font_prop['fontname'],fontsize=15 )
+    ax.set_yticklabels(df['Job Title'].str.title(), alpha=0.7, fontname=font_prop['fontname'], fontsize=15)
     ax.set_xlim(1, df['Salary'].max() + 2000)
     plt.xticks(alpha=0.7)
 
@@ -42,18 +51,27 @@ def creating_a_distributed_dot_plot_for_each_department(df_raw, font_prop):
     plt.gca().spines["right"].set_visible(False)
     plt.gca().spines["left"].set_visible(False)
     plt.grid(axis='both', alpha=.4, linewidth=.1)
+
+    plt.savefig('Salary distribution across HR positions.jpg', dpi=250, bbox_inches='tight')
     plt.show()
 
 
 if __name__ == '__main__':
-
-    df_raw = pd.read_csv('/home/shay_diy/PycharmProjects/Exploring_salary_cross_country/data/salary_by_job_country/hr.csv')
+    df_raw = pd.read_csv(
+        '/home/shay_diy/PycharmProjects/Exploring_salary_cross_country/data/salary_by_job_country/hr.csv')
     font_properties = {'fontsize': 32,
                        'weight': 'heavy',
                        'ha': 'center',
                        'alpha': 0.9,
                        'color': 'Gray',
-                       'fontname':'Franklin Gothic Medium Cond'
+                       'fontname': 'Franklin Gothic Medium Cond'
                        }
 
     creating_a_distributed_dot_plot_for_each_department(df_raw, font_properties)
+
+
+    def format_with_commas(value, pos):
+        return f'{value:,.0f}'
+
+    # For x axis
+    ax.xaxis.set_major_formatter(FuncFormatter(format_with_commas))
