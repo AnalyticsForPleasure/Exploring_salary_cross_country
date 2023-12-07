7882bea7b32911660318980ac8c40ea25cf58c68
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-12-07 09:08:54 +0200

    draw colored scatterplot using a palette colors

--- Questions/Question_6/Age_vs_Salary.py
+++ Questions/Question_6/Age_vs_Salary.py
@@ -3,7 +3,6 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 if __name__ == '__main__':
     pd.set_option('display.max_rows', 500)  # To see all rows
     pd.set_option('display.max_columns', 500)  # To see all columns
@@ -16,10 +15,10 @@ if __name__ == '__main__':
     df['Salary'] = df['Salary'].apply(lambda x: int(x))
     df['Age'] = df['Age'].apply(lambda x: int(x))
 
-    df['coordinates'] = df.apply(lambda row: (row['Salary'], row['Age']), axis=1)
+    # df['coordinates'] = df.apply(lambda row: (row['Salary'], row['Age']), axis=1)
 
-    relevent_data = df.loc[(df['Job Title'] == 'Data Analyst') & (df['Years of Experience'] == 6)]
-    #relevent_data =  df.loc[(df['Job Title'] == 'Data Analyst')]
+    relevent_data = df.loc[(df['Job Title'] == 'Data Analyst') & (df['Years of Experience'] == 3)]
+    # relevent_data =  df.loc[(df['Job Title'] == 'Data Analyst')]
     result = relevent_data['Years of Experience'].value_counts()
     relevent_data.shape[0]
     print('*')
@@ -53,71 +52,63 @@ if __name__ == '__main__':
     plt.rcParams['mathtext.it'] = 'Arial:italic'
 
     # Adding the coordinates
-    # relevent_data['Salary'] = relevent_data['Salary'].apply(lambda x: int(x))
-    # relevent_data['Age'] = relevent_data['Age'].apply(lambda x: int(x))
-    #
-    # relevent_data['coordinates'] = relevent_data.apply(lambda row: (row['Salary'], row['Age']), axis=1)
-    x_values = relevent_data['Gender']
-    y_values =  np.array([relevent_data['coordinates']])
+    x_values = relevent_data['Salary']
+    y_values = relevent_data['Age']
     print('*')
 
     # create new figure
-    fig, ax1 = plt.subplots(figsize=(8.2, 5.4),  # width, height in inches
-                            dpi=110)  # resolution of the figure
+    fig, ax = plt.subplots(figsize=(8.2, 5.4),  # width, height in inches
+                           dpi=110)  # resolution of the figure
 
     # tune the subplot layout by setting sides of the figure
     fig.subplots_adjust(left=0.262, right=0.875, top=0.605, bottom=0.06)
 
     # draw colored scatterplot
-    colors = [GRAY7, RED1, RED1, RED1, GRAY7, RED1] # GRAY7
-    # TODO: need to understand why line 74 is not running
-    #ax1.scatter(y_values[:, 0], y_values[:, 1], color=colors, s=80)
+    colors = [GRAY7, RED1, RED1, RED1, GRAY7, RED1, GRAY8]  # GRAY7
+    colors = plt.cm.Dark2(range(len(y_values)))
+    ax.scatter(x_values, y_values, color=plt.cm.Dark2(range(len(y_values))), s=80)
 
     # annotate with colored labels with offsets
-    txt_colors = [GRAY6, RED1, RED1, RED1, GRAY6, RED1] #  GRAY6
-    offsets = [(10, -4), (-35, -18), (-7, -18), (10, -4),(10, -4), (10, -4)]
+    txt_colors = [GRAY6, RED1, RED1, RED1, GRAY6, RED1]  # GRAY6
 
-    #TODO: need to digure out the problem - > c' argument has 6 elements, which is inconsistent with 'x' and 'y' with size 1.
-    # for i, n in enumerate(x_values):
-    #     ax1.annotate('Worker ' + n,  # Model X text of the annotation
-    #                  y_values[i],  # (x,y) point to annotate
-    #                  xytext=offsets[i],  # (x,y) to place the text at
-    #                  textcoords='offset points',  # offset (in points)
-    #                  color=txt_colors[i],
-    #                  fontsize=11)
+    for i, coord in enumerate(zip(x_values, y_values)):
+        ax.annotate('Worker ' + str(i),  # Model X text of the annotation
+                    xy=coord,  # (x,y) point to annotate
+                    xytext=(0, 0),  # (x,y) to place the text at
+                    textcoords='offset points',  # offset (in points)
+                    color=colors[i],
+                    fontsize=11)
 
     # draw prior year average point
-    ax1.scatter([72], [900], color=GRAY1, s=80)
 
     # draw prior year average vertical and horizontal ref lines
-    plt.axvline(x=72, color=GRAY1, linewidth=0.5)
-    plt.axhline(y=900, color=GRAY1, linewidth=0.5)
+    # plt.axvline(x=72, color=GRAY1, linewidth=0.5)
+    # plt.axhline(y=900, color=GRAY1, linewidth=0.5)
+    #
+    # # annotate prior year average point
+    # ax1.text(64, 880, 'Prior Year Avg.', color=GRAY3, fontsize=12, fontweight='bold')
+    # ax1.text(66.6, 980, '(all models)', color=GRAY3, fontsize=10)
+    #
+    # # set the data limits for the y-axis and x-axis
+    # ax1.set_xlim([350, 19500])  # min and max values for the salaries
+    # ax1.set_ylim([21, 62])  # min and max values for the ages
+    #
+    # # invert y axis
+    ax.invert_yaxis()
+    #
+    # # set properties for axes object
+    # xticks = list(range(60, 95, 5))  # calculate x ticks
+    # yticks = list(range(0, 1600, 200))  # calculate y ticks
+    # plt.setp(ax1,
+    #          xticks=xticks,  # set x ticks
+    #          xticklabels=[str(i) + '%' for i in xticks],  # with n% labels
+    #          yticks=yticks)
+    #
+    # # change the appearance of ticks, tick labels, and gridlines
+    ax.tick_params(top='on', bottom='off', labelbottom='off', labeltop='on')
 
-    # annotate prior year average point
-    ax1.text(64, 880, 'Prior Year Avg.', color=GRAY3, fontsize=12, fontweight='bold')
-    ax1.text(66.6, 980, '(all models)', color=GRAY3, fontsize=10)
-
-    # set the data limits for the y-axis and x-axis
-    ax1.set_xlim([350, 19500]) # min and max values for the salaries
-    ax1.set_ylim([21, 62]) # min and max values for the ages
-
-    # invert y axis
-    ax1.invert_yaxis()
-
-    # set properties for axes object
-    xticks = list(range(60, 95, 5))  # calculate x ticks
-    yticks = list(range(0, 1600, 200))  # calculate y ticks
-    plt.setp(ax1,
-             xticks=xticks,  # set x ticks
-             xticklabels=[str(i) + '%' for i in xticks],  # with n% labels
-             yticks=yticks)
-
-    # change the appearance of ticks, tick labels, and gridlines
-    ax1.tick_params(top='on', bottom='off', labelbottom='off', labeltop='on')
-
-    # remove chart border
-    ax1.spines['right'].set_visible(False)
-    ax1.spines['bottom'].set_visible(False)
-
+    # # remove chart border
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
 
     plt.show()
