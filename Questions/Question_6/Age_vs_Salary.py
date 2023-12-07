682dd5a6810947a79910872e366770b25ad663d0
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-12-07 21:51:24 +0200

    Update to the plot

--- Questions/Question_6/Age_vs_Salary.py
+++ Questions/Question_6/Age_vs_Salary.py
@@ -4,7 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
-
+# **************************************************************************************************************
+# Function  name: my_funct
+# input: entering 3 - x,y of the specific coordinate , mean age and mean salary
+# return value: Returning the relative color for each point
+# ***************************************************************************************************************
 def my_funct(my_tup, mean_age, mean_salary):
     age, salary = my_tup
     if (age > mean_age) & (salary > mean_salary):
@@ -20,6 +24,63 @@ def my_funct(my_tup, mean_age, mean_salary):
 # Define a function to format tick labels with commas
 def format_ticks_with_commas(value, _):
     return f"{value:,.0f}$"
+
+
+def creating_a_scatter_avg_chart(df, font_prop):
+
+    global mean_salary_ds, mean_age_ds
+    df['Salary'] = df['Salary'].apply(lambda x: int(x))
+    df['Age'] = df['Age'].apply(lambda x: int(x))
+    df['Years of Experience'] = df['Years of Experience'].apply(lambda x: int(x))
+    print(df['Job Title'].value_counts())
+    relevant_data = df.loc[(df['Years of Experience'] <= 15) & (df['Job Title'] == 'Software Engineer')]
+    min_age = df['Age'].min()
+    max_age = df['Age'].max()
+    min_age_for_a_data_analyst = relevant_data['Age'].min()
+    max_age_for_a_data_analyst = relevant_data['Age'].max()
+    min_salary = df['Salary'].min()
+    max_salary = df['Salary'].max()
+    min_salary_for_a_data_analyst = relevant_data['Salary'].min()
+    max_salary_for_a_data_analyst = relevant_data['Salary'].max()
+    x_values = relevant_data['Salary']
+    y_values = relevant_data['Age']
+    # create new figure
+    fig, ax = plt.subplots(figsize=(8.2, 5.4),  # width, height in inches
+                           dpi=110)  # resolution of the figure
+    mean_salary_ds = relevant_data['Salary'].mean()
+    mean_age_ds = relevant_data['Age'].mean()
+    relevant_data['color'] = relevant_data.loc[:, ['Age', 'Salary']].apply(
+        lambda pair: my_funct(pair, mean_age_ds, mean_salary_ds), axis=1)
+    ax.scatter(x_values, y_values, color=relevant_data['color'], s=80)
+    ax.set_xlabel("Salary")
+    ax.set_ylabel("Age")
+    # Apply the formatter to the x-axis
+    ax.xaxis.set_major_formatter(FuncFormatter(format_ticks_with_commas))
+    plt.axvline(x=mean_salary_ds, linewidth=0.5)  # color=gray,
+    plt.axhline(y=relevant_data['Age'].mean(), linewidth=0.5)  # color=GRAY1,
+    ax.annotate('My avg Salary',
+                xy=(mean_salary_ds, 30))
+    ax.annotate('My avg Age',
+                xy=(100000, mean_age_ds))
+    ax.scatter([mean_salary_ds], [mean_age_ds],
+               marker='o',
+               linestyle='dashed',
+               s=250,
+               facecolor='none',
+               edgecolor='blue')
+
+    ax.set_title('Distribution of Salary over a specific job roll',
+                fontsize=font_prop['fontsize'],
+                fontname=font_prop['fontname'],
+                color='Black')
+    # invert y axis
+    ax.invert_yaxis()
+    # change the appearance of ticks, tick labels, and gridlines
+    ax.tick_params(top='on', bottom='off', labelbottom='off', labeltop='on')
+    # # remove chart border
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -38,65 +99,12 @@ if __name__ == '__main__':
 
     # ['Age', 'Gender', 'Education Level', 'Job Title', 'Years of Experience', 'Salary', 'Country', 'Race', 'Senior']
 
-    df['Salary'] = df['Salary'].apply(lambda x: int(x))
-    df['Age'] = df['Age'].apply(lambda x: int(x))
-    df['Years of Experience'] = df['Years of Experience'].apply(lambda x: int(x))
-    print(df['Job Title'].value_counts())
-    relevant_data = df.loc[(df['Years of Experience'] <= 10) & (df['Job Title'] == 'Data Analyst')]
+    font_properties = {'fontsize': 22,
+                       'weight': 'heavy',
+                       'ha': 'center',
+                       'alpha': 0.9,
+                       'color': 'Gray',
+                       'fontname':'Franklin Gothic Medium Cond'
+                       }
 
-    min_age = df['Age'].min()
-    max_age = df['Age'].max()
-    min_age_for_a_data_analyst = relevant_data['Age'].min()
-    max_age_for_a_data_analyst = relevant_data['Age'].max()
-
-    min_salary = df['Salary'].min()
-    max_salary = df['Salary'].max()
-    min_salary_for_a_data_analyst = relevant_data['Salary'].min()
-    max_salary_for_a_data_analyst = relevant_data['Salary'].max()
-
-    x_values = relevant_data['Salary']
-    y_values = relevant_data['Age']
-
-    # create new figure
-    fig, ax = plt.subplots(figsize=(8.2, 5.4),  # width, height in inches
-                           dpi=110)  # resolution of the figure
-
-    mean_salary_ds = relevant_data['Salary'].mean()
-    mean_age_ds = relevant_data['Age'].mean()
-
-    relevant_data['color'] = relevant_data.loc[:, ['Age', 'Salary']].apply(
-        lambda pair: my_funct(pair, mean_age_ds, mean_salary_ds), axis=1)
-
-    ax.scatter(x_values, y_values, color=relevant_data['color'], s=80)
-    ax.set_xlabel("Salary")
-    ax.set_ylabel("Age")
-
-    # Apply the formatter to the x-axis
-    ax.xaxis.set_major_formatter(FuncFormatter(format_ticks_with_commas))
-    plt.axvline(x=mean_salary_ds, linewidth=0.5)  # color=gray,
-    plt.axhline(y=relevant_data['Age'].mean(), linewidth=0.5)  # color=GRAY1,
-
-    ax.annotate('My avg Salary',
-                xy=(mean_salary_ds, 30))
-
-    ax.annotate('My avg Age',
-                xy=(100000, mean_age_ds))
-
-    ax.scatter([mean_salary_ds], [mean_age_ds],
-               marker='o',
-               linestyle='dashed',
-               s=250,
-               facecolor='none',
-               edgecolor='blue')
-
-    # invert y axis
-    ax.invert_yaxis()
-
-    # change the appearance of ticks, tick labels, and gridlines
-    ax.tick_params(top='on', bottom='off', labelbottom='off', labeltop='on')
-
-    # # remove chart border
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-
-    plt.show()
+    res = creating_a_scatter_avg_chart(df, font_properties)
